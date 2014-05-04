f4cf526478e678a5f178817f27a411bbf9265972
Benjamin Hiltpolt <benjamin.hiltpolt@gmail.com>
2014-05-04 17:51:41 +0200

    showing images for level

--- KivyLogicApp.py
+++ KivyLogicApp.py
@@ -41,7 +41,17 @@ class PlayingField(GridLayout):
 
         for x in xrange(level.size):
             for y in xrange(level.size):
-                self.add_widget(Label(text= level.data[x][y]))
+                if level.data[x][y]=="#":
+                    wimg = Image(source="images/block_collectable.png")
+                elif level.data[x][y]=="X":
+                    wimg = Image(source="images/block_death.png")
+                elif level.data[x][y]=="_":
+                    wimg = Image(source="images/block_blank.png")
+                elif level.data[x][y]=="M":
+                    wimg = Image(source="images/block_robot.png")
+                else:
+                    wimg = Image(source="images/block_wall.png")
+                self.add_widget(wimg)
 
 
 class KivyLogicApp(App):
