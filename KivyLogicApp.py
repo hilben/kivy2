8ef9dfec5a036573ddb5ff397e55b881bbbcb917
Benjamin Hiltpolt <benjamin.hiltpolt@gmail.com>
2014-05-04 17:33:22 +0200

    displaying some pictures for the logic blocks

--- KivyLogicApp.py
+++ KivyLogicApp.py
@@ -6,47 +6,48 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
-
-from kivyrunner import KivyRunner
+from kivy.uix.image import Image
+from robocoder.kivyrunner import KivyRunner
 
 class MainLayout(BoxLayout):
-	def __init__(self, **kwargs):  
-		super(MainLayout, self).__init__(**kwargs)
-			#self.kivyrunner = KivyRunner()
+    def __init__(self, **kwargs):  
+        super(MainLayout, self).__init__(**kwargs)
+        #self.kivyrunner = KivyRunner()
 
 class GameLogic(FloatLayout):
-	def __init__(self, **kwargs):  
-		super(GameLogic, self).__init__(**kwargs)
+    def __init__(self, **kwargs):  
+        super(GameLogic, self).__init__(**kwargs)
 
 class LogicBlocksGridLayout(GridLayout):
-	def __init__(self, **kwargs):
-		super(LogicBlocksGridLayout, self).__init__(**kwargs)
-		#print  "AAAAAAAAAAAAAAAAAAAAAA" + str(self.ids)
-		self.kivyrunner = KivyRunner()
-		logic_boxes = self.kivyrunner.getLogicBoxes()
+    def __init__(self, **kwargs):
+        super(LogicBlocksGridLayout, self).__init__(**kwargs)
+        #print  "AAAAAAAAAAAAAAAAAAAAAA" + str(self.ids)
+        self.kivyrunner = KivyRunner()
+        logic_boxes = self.kivyrunner.getLogicBoxes()
 
-		for logic_box in logic_boxes:
-			pass
-			self.add_widget(Label(text= logic_box.picture))
+        #TODO: use OS.Lineseperator
+        for logic_box in logic_boxes:
+            wimg = Image(source="images/"+logic_box.picture)
+            self.add_widget(wimg)
 
 
 class PlayingField(GridLayout):
-	def __init__(self, **kwargs):  
-		super(PlayingField, self).__init__(**kwargs)
-		self.kivyrunner = KivyRunner()
-		self.kivyrunner.loadLevel(1)
+    def __init__(self, **kwargs):  
+        super(PlayingField, self).__init__(**kwargs)
+        self.kivyrunner = KivyRunner()
+        self.kivyrunner.loadLevel(1)
 
-		level = self.kivyrunner.level
+        level = self.kivyrunner.level
 
-		for x in xrange(level.size):
-			for y in xrange(level.size):
-			    self.add_widget(Label(text= level.data[x][y]))
+        for x in xrange(level.size):
+            for y in xrange(level.size):
+                self.add_widget(Label(text= level.data[x][y]))
 
 
 class KivyLogicApp(App):
 
     def build(self):
-    	return MainLayout()
+        return MainLayout()
 
 if __name__ == "__main__":
     KivyLogicApp().run()
